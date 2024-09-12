99f5c0f78d69524b4fc9bf529d152ef0b3d5ea6d
markoc1997 <markocakmak55@gmail.com>
2024-09-12 16:57:45 +0200

    Domaci vezbanje,relacije

--- vezbanje/app/Models/Ocene.php
+++ vezbanje/app/Models/Ocene.php
@@ -11,6 +11,7 @@ class Ocene extends Model
     protected $fillable = [
         "predmet",
         "ocena",
-        "profesor"
+        "profesor",
+        "user_id"
     ];
 }
--- vezbanje/routes/web.php
+++ vezbanje/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\OceneController;
+use App\Http\Controllers\ProductsController;
 use App\Models\Ocene;
 use Illuminate\Support\Facades\Route;
 
@@ -8,6 +9,8 @@ Route::get('/', function () {
     $ocene = Ocene::all();
     return view("welcome", compact("ocene"));
 });
+
+Route::get("/admin/all-products", [ProductsController::class, "index"]);
 
 Route::view("/dodaj-ocenu", "addGrade");
 Route::post("/add-user-grade", [OceneController::class, "addGrade"]);
